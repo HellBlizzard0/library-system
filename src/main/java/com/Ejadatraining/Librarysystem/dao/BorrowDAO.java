1599f3fee3ea2b0f9f3140a642b9e3bed397370b
HellBlizzard <hellblizzard5@gmail.com>
2022-03-03 11:46:00 +0300

    Fixed customer reference for Borrow

--- src/main/java/com/Ejadatraining/Librarysystem/dao/BorrowDAO.java
+++ src/main/java/com/Ejadatraining/Librarysystem/dao/BorrowDAO.java
@@ -3,6 +3,7 @@ package com.Ejadatraining.Librarysystem.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Ejadatraining.Librarysystem.entity.Borrow;
+import com.Ejadatraining.Librarysystem.entity.Customer;
 
 /**
  *
@@ -13,5 +14,7 @@ public interface BorrowDAO extends JpaRepository<Borrow, Integer> {
 	// public Borrow findByUserId(Integer userId);
 
     public Borrow findByBookId(Integer bookId);
+
+    public Borrow findByCustomer(Customer customer);
 
 }
