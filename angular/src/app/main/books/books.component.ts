28e340fa5dd90c50f30742c1d1cc29541a0abd97
HellBlizzard <hellblizzard5@gmail.com>
2022-05-08 09:50:02 +0300

    pre-ramadan

--- angular/src/app/main/books/books.component.ts
+++ angular/src/app/main/books/books.component.ts
@@ -163,6 +163,7 @@ export class BooksComponent implements OnInit {
 
   printReport() {
     console.log();
+    this.books[0].dateOfCreation?.getMilliseconds();
 
     this.bookService.printReport(this.selectedBooks);
   }
